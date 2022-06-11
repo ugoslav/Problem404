73491077b1543ca2c8b358b9cde75a6bd0570dd6
Subharup Chakraborty <82225359+ugoslav@users.noreply.github.com>
2022-06-12 00:34:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ExerciseTwo from './components/exerciseTwo';
+
+{/*import {notes, secondNotes} from "./components/database";
+import GamesList from "./components/gamesList";
+
+
+const totalNotes = [...notes , ...secondNotes]*/}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <>
+      <ExerciseTwo />
+    </>
+);
